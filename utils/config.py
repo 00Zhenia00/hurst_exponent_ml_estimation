65876df8268387096a4e67373e00e1e7f8e49c46
Yevhenii_Sheptukha <e.i.sheptukha@gmail.com>
2025-08-25 20:12:08 +0300

    Add config file

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+import os
+import yaml
+from types import SimpleNamespace
+
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.yaml')
+
+
+def load_config():
+    with open(CONFIG_PATH) as file:
+        config = yaml.safe_load(file)
+    return SimpleNamespace(**config)
